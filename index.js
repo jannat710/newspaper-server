3be44bc72d651ebdbff491b805dab942d8433746
e <jannatul0040@gmail.com>
2023-11-29 00:57:33 +0600

    installed jwt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 
 // middleware
 app.use(cors());
