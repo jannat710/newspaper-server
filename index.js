9e17f3a9e3cd71a496fe5985542299406abcc5fb
e <jannatul0040@gmail.com>
2023-11-29 00:23:53 +0600

    delete user from the database

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const port = process.env.PORT || 5001;
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pvt8fts.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -44,6 +44,14 @@ async function run() {
                 return res.send({ message: 'user already exists', insertedId: null })
             }
             const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        //delete user
+        app.delete('/users/:id',async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
             res.send(result);
         })
 
